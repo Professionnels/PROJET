0a2e9582a5831e13d55e6a6f74333f7d7865b6a7
El Parrain <louis.kedemos@free.fr>
2014-02-10 14:20:15 +0100

    code de lenny
    code de lenny pour les classes decors et perso

---
+++ ANNEXES/code lenny/Decors/Escalier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evasion.Decors
+{
+    class Escalier
+    {
+        private string fichier3D;
+        private int type;
+        private int identifiant;
+
+        public Escalier() { }
+        public Escalier(string fichier3D, int type, int identifiant) 
+        {
+            this.fichier3D = fichier3D;
+            this.type = type;
+            this.identifiant = identifiant;
+        }
+    }
+}
