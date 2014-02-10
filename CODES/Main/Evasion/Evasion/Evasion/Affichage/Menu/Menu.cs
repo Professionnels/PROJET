120d9f0aab7e5b68c7ccda2a00b339cf1303e22a
Totonut <anatolemoreau@hotmail.fr>
2014-02-10 03:56:15 +0100

    Ajout du Menu (Debug non fini). Rajout Des Constantes d'affichage. Structuration de la Fenetre. Rename des Graphismes.

--- CODES/Main/Evasion/Evasion/Evasion/Affichage/Menu/Menu.cs
+++ CODES/Main/Evasion/Evasion/Evasion/Affichage/Menu/Menu.cs
@@ -2,30 +2,97 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using System.Reflection;
+using System.IO;
+using System.Drawing;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
+
 
 namespace Evasion.Affichage.Menu
 {
-    class Menu : Game1
+    class Menu
     {
-        private Texture2D Image_Fond;
-        private Texture2D[] Liste_boutons;
+        private Texture2D Image;
+        private char Id;
+        private Bouton[] Elements;
+        private string type;
+        private Fenetre Fen;
+        private string path;
+        private Microsoft.Xna.Framework.Game screen;
 
-        public Menu(string type)
+        public Menu(string type, Fenetre Fen)
         {
-            Image_Fond = Content.Load<Texture2D>("menu_accueil_fond");
-            //this.Liste_boutons = ArbreMenu.Get_Boutons(type);
+            this.type = type;
+            this.Fen = Fen;
         }
 
-        public void Affiche_Menu(SpriteBatch sb)
+        public void LoadContent(Microsoft.Xna.Framework.Game screen)
         {
-            sb.Draw(Image_Fond, new Vector2(0, 0), new Rectangle(0, 0, 2000, 1000), Color.White);
+            this.screen = screen;
+            switch (type)
+            {
+                case "Main menu":
+                    Elements = new Bouton[4] { new Bouton("New game"), new Bouton("Load game"), new Bouton("Quit"), new Bouton("Options") };
+                    path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName + "\\GRAPHISMES\\Images\\Menu\\Menu_accueil_fond.bmp");
+                    Image = screen.Content.Load<Texture2D>(path);
+                    break;
+                case "Pause":
+                    Elements = new Bouton[4] { new Bouton("Save"), new Bouton("Quit"), new Bouton("Main menu"), new Bouton("Options") };
+                    path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName + "\\GRAPHISMES\\Images\\Menu\\Menu_accueil_fond.bmp");
+                    Image = screen.Content.Load<Texture2D>(path);
+                    break;
+                case "Quit":
+                    Fen.LoadContent(Content_t.Quit);
+                    break;
+                case "Save":
+                    Fen.LoadContent(Content_t.Save);
+                    break;
+                case "New game":
+                    Fen.LoadContent(Content_t.NewGame);
+                    break;
+                case "Load game":
+                    Fen.LoadContent(Content_t.LoadGame);
+                    break;
+                case "Options":
+                    Fen.LoadContent(Content_t.Options);
+                    break;
+            }
+        }
+
+        public void Update(KeyboardState keyboardState, MouseState mouseState)
+        {
+            for (int i = 0; i < Elements.Length; i++)
+            {
+                if (!(Elements[i].Gris) && mouseState.Y >= Elements[i].Pos.Y && mouseState.Y <= Elements[i].Pos.Y + Elements[i].size.Height)
+                    Elements[i].Griser();
+                else if (Elements[i].Gris && (mouseState.Y < Elements[i].Pos.Y || mouseState.Y > Elements[i].Pos.Y + Elements[i].size.Height))
+                    Elements[i].DeGriser();
+                if (mouseState.LeftButton == ButtonState.Pressed && mouseState.Y >= Elements[i].Pos.Y && mouseState.Y <= Elements[i].Pos.Y + Elements[i].size.Height)
+                {
+                    type=Elements[i].Name;
+                    LoadContent(screen);
+                }
+            }
+        }
+
+        public void Display(Microsoft.Xna.Framework.Game screen, SpriteBatch sb)
+        {
+            sb.Draw(Image, new Vector2(0,0), Microsoft.Xna.Framework.Color.Transparent);
+
+            int height = screen.GraphicsDevice.Viewport.Height;
+            int width = screen.GraphicsDevice.Viewport.Width;
+            Vector2 pos = new Vector2(0, height / (Elements.Length*2));
+            for (int i = 0; i < Elements.Length; i++)
+            {
+                pos.Y += screen.GraphicsDevice.Viewport.Height / (Elements.Length+1);
+                Elements[i].Pos = pos;
+                int pourcentage = 100*height/Elements[i].size.Height;
+                Elements[i].size = new Size(width, Elements[i].size.Height + pourcentage*Elements[i].size.Height/100);
+                Elements[i].Display(screen.Content, sb);
+            }
         }
 
     }
--- CODES/Main/Evasion/Evasion/Evasion/Game1.cs
+++ CODES/Main/Evasion/Evasion/Evasion/Game1.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using Evasion.Affichage;
 
 namespace Evasion
 {
@@ -18,15 +19,18 @@ namespace Evasion
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Evasion.Affichage.Menu.Menu menu; 
-        
-        //Personnage perso = new Personnage();
+       // Personnage perso = new Personnage();
+        Fenetre fenetre;
 
         public Game1()
         {
+            fenetre = new Fenetre(this);
             graphics = new GraphicsDeviceManager(this);
-            this.graphics.IsFullScreen = true;
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = Evasion.Affichage.Constantes.SCREEN_WIDTH;
+            graphics.PreferredBackBufferHeight = Evasion.Affichage.Constantes.SCREEN_HEIGHT;
+            this.Window.Title = "Evasion";
+           // this.graphics.ApplyChanges();
         }
 
         /// <summary>
@@ -40,7 +44,6 @@ namespace Evasion
             // TODO: Add your initialization logic here
 
             base.Initialize();
-            menu = new Evasion.Affichage.Menu.Menu("A");
         }
 
         /// <summary>
@@ -51,7 +54,7 @@ namespace Evasion
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            fenetre.LoadContent(Content_t.Menu);
             // TODO: use this.Content to load your game content here
         }
 
@@ -74,10 +77,13 @@ namespace Evasion
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-            
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+                this.graphics.IsFullScreen = !(this.graphics.IsFullScreen);
+            fenetre.Update(Keyboard.GetState(), Mouse.GetState());
             // TODO: Add your update logic here
 
-            //perso.ecrire();
             base.Update(gameTime);
         }
 
@@ -90,9 +96,9 @@ namespace Evasion
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            fenetre.Display(spriteBatch);
             base.Draw(gameTime);
-            menu.Affiche_Menu(spriteBatch);
         }
     }
 }
