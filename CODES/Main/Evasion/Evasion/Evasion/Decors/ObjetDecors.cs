8863b807489c07b79e2fa88ce80c9bb2205f376a
Les Professionnels <k.chalabi@hotmail.fr>
2014-03-10 14:57:35 +0100

    Decors
    ObjetsDecors finie
    Murs finie

--- CODES/Main/Evasion/Evasion/Evasion/Decors/ObjetDecors.cs
+++ CODES/Main/Evasion/Evasion/Evasion/Decors/ObjetDecors.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Evasion.Decors
 {
     class ObjetDecors
     {
+        private string fichier3D;
+        private int type;
+        private int identifiant;
+        private Vector3 position;
+
+        public string _fichier3D { get { return fichier3D; } }
+        public int _type { get { return type; } }
+        public int _identifiant { get { return identifiant; } }
+        public Vector3 _position { get { return position; } }
+
+        public ObjetDecors(string fichier3D, int type, int identifiant, Vector3 position)
+        {
+            this.fichier3D = fichier3D;
+            this.type = type;
+            this.identifiant = identifiant;
+            this.position = position;
+        }
     }
 }
