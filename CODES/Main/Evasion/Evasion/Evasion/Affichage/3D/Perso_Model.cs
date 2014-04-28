7ba2b9d1e294169a6ca9bface786581b365aefcb
El Parrain <louis.kedemos@free.fr>
2014-04-28 14:39:14 +0200

    brouillon

---
+++ CODES/Main/Evasion/Evasion/Evasion/Affichage/3D/Perso_Model.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Evasion.Affichage;
+
+namespace Evasion.Affichage._3D
+{
+    class Perso_Model
+    {
+        private Model persoModel;
+        private Vector3 persoPosition;
+
+        private Matrix viewMatrix;
+        private Matrix projectionMatrix;
+        private Vector3 rotation;
+
+        private Matrix orientation = Matrix.Identity;
+
+        private float aspectRatio;
+        private Vector3 cameraPosition;
+
+        private float scale = 10f;
+
+        public Vector3 getPosition()
+        {
+            return persoPosition;
+        }
+
+        public Perso_Model(Model model, Vector3 position, Vector3 rotation)
+        {
+            this.persoModel = model;
+            this.persoPosition = position;
+            this.rotation = rotation;
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(40.0f), aspectRatio, 100.0f, 10000.0f);
+            viewMatrix = Matrix.CreateLookAt(cameraPosition, persoPosition, Vector3.Up);
+        }
+
+        private void initPerso()
+        {
+            persoPosition = new Vector3(0f, 3f, 0f);
+            rotation = new Vector3(90.0f, 0f, 180f);
+        }
+
+        public void draw()
+        {
+            Matrix[] transforms = new Matrix[persoModel.Bones.Count];
+            persoModel.CopyAbsoluteBoneTransformsTo(transforms);
+            foreach (ModelMesh mesh in persoModel.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = transforms[mesh.ParentBone.Index] * Matrix.CreateTranslation(persoPosition) *
+                        Matrix.CreateScale(scale) * Matrix.CreateFromAxisAngle(orientation.Right, (float)MathHelper.ToRadians(rotation.X)) *
+                        Matrix.CreateFromAxisAngle(orientation.Up, (float)MathHelper.ToRadians(rotation.Y)) * Matrix.CreateFromAxisAngle(orientation.Forward, (float)MathHelper.ToRadians(rotation.Z));
+
+                    effect.View = viewMatrix;
+                    effect.Projection = projectionMatrix;
+                }
+                mesh.Draw();
+            }
+        }
+
+        public void updatePosition(GameTime gameTime, KeyboardState keyboardState)
+        {
+            float time = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            float vitesse = 0.01f;
+
+
+            if (keyboardState.IsKeyDown(Keys.Right))
+                persoPosition.X -= time * vitesse;
+            if (keyboardState.IsKeyDown(Keys.Left))
+                persoPosition.X += time * vitesse;
+
+
+            if (keyboardState.IsKeyDown(Keys.Down))
+                persoPosition.Y += time * vitesse;
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+                persoPosition.Y -= time * vitesse;
+        }
+
+    }
+}
--- CODES/Main/Evasion/Evasion/Evasion/Game1.cs
+++ CODES/Main/Evasion/Evasion/Evasion/Game1.cs
@@ -19,16 +19,11 @@ namespace Evasion
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-       // Personnage perso = new Personnage();
+        // Personnage perso = new Personnage();
         Fenetre fenetre;
 
         //PERSONNAGE
-        private Model persoModel;
-        private Vector3 persoPosition;
-
-        private Vector3 Rotation;
-        private Vector3 Position;
-
+        Affichage._3D.Perso_Model personnage;
         KeyboardState currentKeyboardState = new KeyboardState();
 
         private Vector3 cameraPosition;
@@ -76,6 +71,8 @@ namespace Evasion
         {
             // TODO: Add your initialization logic here
             this.IsMouseVisible = true;
+            Model modelperso = Content.Load<Model>("Models/perso");
+            personnage = new Affichage._3D.Perso_Model(modelperso, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
             InitPhysique();
             base.Initialize();
         }
@@ -132,7 +129,8 @@ namespace Evasion
             fenetre.Update(Keyboard.GetState(), Mouse.GetState());
             // TODO: Add your update logic here
 
-            UpdatePosition(gameTime);
+            //UpdatePosition(gameTime);
+            personnage.updatePosition(gameTime, Keyboard.GetState());
 
             base.Update(gameTime);
         }
@@ -150,6 +148,7 @@ namespace Evasion
             {
                 GraphicsDevice.DepthStencilState = DepthStencilState.Default;
                 DrawMeshes();
+                
             }
             else
             {
@@ -160,32 +159,10 @@ namespace Evasion
             base.Draw(gameTime);
         }
 
-        private void UpdatePosition(GameTime gameTime)
-        {
-            currentKeyboardState = Keyboard.GetState();
-
-            float time = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            float vitesse = 0.01f;
-
-
-            if (currentKeyboardState.IsKeyDown(Keys.Right))
-                Rotation.Y -= time * vitesse * 10;
-            if (currentKeyboardState.IsKeyDown(Keys.Left))
-                Rotation.Y += time * vitesse * 10;
-
-
-            if (currentKeyboardState.IsKeyDown(Keys.Down))
-                persoPosition.Y += time * vitesse;
-
-            if (currentKeyboardState.IsKeyDown(Keys.Up))
-                persoPosition.Y -= time * vitesse;
-
-
-        }
+       
 
         private void InitPhysique()
         {
-            persoPosition = new Vector3(0f, 3f, 0f);
             murPosition = new Vector3(1f, 0f, -1.37f);
             TmurPosition = new Vector3(-1f, 0f, -1.2f);
             solPosition = new Vector3(0f, 0f, -0.28f);
@@ -196,19 +173,16 @@ namespace Evasion
 
             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(40.0f), aspectRatio, 100.0f, 10000.0f);
 
-            Position = Vector3.Zero;
-            Rotation = new Vector3(90.0f, 0f, 180f);
 
             murRotation = new Vector3(90.0f, 0f, 180f);
             TmurRotation = new Vector3(90.0f, 0f, 180f);
             solRotation = new Vector3(90.0f, 0f, 180f);
 
-            viewMatrix = Matrix.CreateLookAt(cameraPosition, persoPosition, Vector3.Up);
+            viewMatrix = Matrix.CreateLookAt(cameraPosition, personnage.getPosition(), Vector3.Up);
         }
 
         private void LoadModel()
         {
-            persoModel = Content.Load<Model>("Models/perso");
             mur = Content.Load<Model>("Models/mur");
             Tmur = Content.Load<Model>("Models/murtableau");
             sol = Content.Load<Model>("Models/sol");
@@ -216,23 +190,7 @@ namespace Evasion
 
         private void DrawMeshes()
         {
-            Matrix[] transforms = new Matrix[persoModel.Bones.Count];
-            persoModel.CopyAbsoluteBoneTransformsTo(transforms);
-            foreach (ModelMesh mesh in persoModel.Meshes)
-            {
-                foreach (BasicEffect effect in mesh.Effects)
-                {
-                    effect.EnableDefaultLighting();
-                    effect.World = transforms[mesh.ParentBone.Index] * Matrix.CreateTranslation(persoPosition) *
-                        Matrix.CreateScale(scale) * Matrix.CreateFromAxisAngle(orientation.Right, (float)MathHelper.ToRadians(Rotation.X)) *
-                        Matrix.CreateFromAxisAngle(orientation.Up, (float)MathHelper.ToRadians(Rotation.Y)) * Matrix.CreateFromAxisAngle(orientation.Forward, (float)MathHelper.ToRadians(Rotation.Z));
-
-                    effect.View = viewMatrix;
-                    effect.Projection = projectionMatrix;
-                }
-                mesh.Draw();
-
-            }
+            
 
             foreach (ModelMesh mesh in mur.Meshes)
             {
