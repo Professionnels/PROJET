8e93360c02fc7220040296fe114334b4441c0e02
Les Professionnels <k.chalabi@hotmail.fr>
2014-05-01 14:17:36 +0200

    barre vie

---
+++ CODES/Main/Evasion/Evasion/Evasion/Affichage/Informations/BarreVie.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace Evasion.Affichage.Informations
+{
+    class BarreVie
+    {
+        private int vieMax;
+        private int vieActu;
+        private float tailleBarre;
+        private float tailleVie;
+        public Texture2D barre;
+
+        public BarreVie(int vieMax, int vieActu, float tailleBarre, ContentManager Content)
+        {
+            this.vieMax = vieMax;
+            this.vieActu = vieActu;
+            this.tailleBarre = tailleBarre;
+            this.barre = Content.Load<Texture2D>("ImageBarreVie");
+            this.tailleVie = tailleBarre;
+        }
+
+        public void AjusteBarreVie()
+        {
+            if (vieActu > vieMax)
+                vieActu = vieMax;
+
+            if (vieActu < 0)
+                vieActu = 0;
+
+        }
+
+        public void UpdateVie(int ajout)
+        {
+            vieActu = vieActu + ajout;
+        }
+
+        private void UpdateTailleVie()
+        {
+            tailleVie = (tailleBarre) * ((float)vieActu / (float)vieMax);
+        }
+
+        public void Draw()
+        {
+ 
+        }
+    }
+}
