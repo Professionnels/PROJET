c4a6b4e4ed2850fb57389dd9aeeb99799071b692
lenny005 <lennydanino@hotmail.fr>
2014-05-02 16:13:08 +0200

    pnj,rapport de stage graphiques

--- CODES/Main/Evasion/Evasion/Evasion/Personnages/Pnj.cs
+++ CODES/Main/Evasion/Evasion/Evasion/Personnages/Pnj.cs
@@ -2,10 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 
 namespace Evasion.Personnages
 {
-    class Pnj
+    class Pnj : Personnage
     {
+        private string dialogue1;
+        private string dialogue2;
+        private string dialogue3;
+        private int objet_a_donner;
+
+        public string _dialogue1 { get { return dialogue1; } }
+        public string _dialogue2 { get { return dialogue2; } }
+        public string _dialogue3 { get { return dialogue3; } }
+        public int _objet_a_donner { get { return objet_a_donner; } } 
+
+    public Pnj(ContentManager Content, Matrix view, float aspectRatio)
+            : base("Pnj", 100, deplacement_t.marche, new Vector3(0,0,0), 1, "fichier3D", genre_t.pnj, Content, view, aspectRatio)
+        {
+ 
+        }
+
+        public Pnj(string nom, int vie, deplacement_t deplacement, Vector3 position, int vitesse, int id, string fichier3D, string dialogue1, string dialogue2, string dialogue3, int objet_a_donner, ContentManager Content, Matrix view, float aspectRatio)
+            : base(nom, vie, deplacement, position, vitesse, fichier3D, genre_t.pnj, Content, view, aspectRatio) // fichier mouv.txt
+        {
+            this.dialogue1 = dialogue1;
+            this.dialogue2 = dialogue2;
+            this.dialogue3 = dialogue3;
+        }
+
+        public Action()
+        {
+
+        }
+
+        public string Parler()
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+            if(Keyboard.GetState().IsKeyDown(Keys.Enter))
+                this.
+                
+        }
+        public int Echanger(){}
     }
 }
